2a35fb9e9dd077d5ed09b75b0896aa4df8a530f1
p00ria <amir1300sobatdar@gmail.com>
2016-03-28 23:42:09 +0430

    Update Banhammer.lua

--- plugins/Banhammer.lua
+++ plugins/Banhammer.lua
@@ -131,7 +131,7 @@ local function run(msg, matches)
     elseif matches[1]:lower() == 'id' then
       local name = user_print_name(msg.from)
       savelog(msg.to.id, name.." ["..msg.from.id.."] used /id ")
-      return "Group ID for " ..string.gsub(msg.to.print_name, "_", " ").. ":\n\n"..msg.to.id  
+      return "👥️ Group ID for " ..string.gsub(msg.to.print_name, "_", " ").. ":\n"..msg.to.id.. "\n👥 User ID for " ..string.gsub(msg.from.print_name, "_", " ").. ":\n"..msg.from.id 
     end
   end
   if matches[1]:lower() == 'kickme' then-- /kickme
